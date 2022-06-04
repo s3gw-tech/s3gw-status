bcefb31403ac80917c5e0efcd55a5afc50ea4e84
Joao Eduardo Luis <joao@suse.com>
2022-06-04 13:02:38 +0000

    s3gw-status: initial commit
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

---
+++ s3gw-status/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 's3gw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 's3gw-status';
+}
